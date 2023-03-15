5e6151519b0f9fb9d6c41c9c5b787c2951776b2c
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-03-16 00:55:12 +0300

    final result

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hello from "./components/Hello.js";
+
+function App() {
+    return (
+        <div style={{backgroundColor: '#46B1DE'}} className="wrapper">
+            <Hello />
+            <button onClick={() => console.log('Click on button')}>Button</button>
+        </div>
+    );
+}
+
+export default App;
